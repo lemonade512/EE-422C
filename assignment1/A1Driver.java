9b9ab60e7eb340023207e6eb10d0f969eefb8196
Phillip Lemons <Phillip Lemons@PhillipLemons>
2014-09-02 21:39:23 -0500

    First Commit

---
+++ assignment1/A1Driver.java
@@ -0,0 +1,14 @@
+/**
+ * Class to find palindromes in input text.
+ * Solves EE422C programming assignment #1
+ * Student Name: Phillip Lemons, Lab Section: 16830
+ * @author Phillip Lemons
+ * @version 0.01 2010-08-15
+ */
+package assignment1;
+
+public class A1Driver {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+}
---
+++ assignment1/README.md
@@ -0,0 +1,3 @@
+<h1>Assignment 1</h1>
+This assignment is to find palindromes hidden inside of strings.
+* Input lines are <= 60 characters
