40d6a00edb9132cd24063c3a0872fdf207da6ef1
Phillip Lemons <Phillip Lemons@PhillipLemons>
2014-09-09 22:39:34 -0500

    Finished and cleaned up

--- Assignment1/A1Driver.java
+++ Assignment1/A1Driver.java
@@ -1,11 +1,3 @@
-/**
- * Class to find palindromes in input text.
- * Solves EE422C programming assignment #1
- * Student Name: Phillip Lemons, Lab Section: 16830
- * @author Phillip Lemons
- * @version 0.01 2010-08-15
- */
-
 package Assignment1;
 
 import java.io.BufferedReader;
@@ -15,12 +7,22 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.TreeSet;
 
-public class A1Driver {
+/**
+ * Class to find palindromes in input text. Solves EE422C programming assignment #1
+ * Student Name: Phillip Lemons, Lab Section: 16830
+ * 
+ * @author Phillip Lemons
+ * @version 1.0 2010-08-15
+ */
 
-	public static void main(String args[]) {
-		if (args.length != 1) {
-			System.err
-					.println("Error: Incorrect number of command line arguments");
+public class A1Driver
+{
+
+	public static void main(String args[])
+	{
+		if (args.length != 1)
+		{
+			System.err.println("Error: Incorrect number of command line arguments");
 			System.exit(-1);
 		}
 		processLinesInFile(args[0]);
@@ -32,26 +34,30 @@ public class A1Driver {
 	 * Invokes parse () on each line in the file, and prints out the string with
 	 * all discovered palindromes in it.
 	 * 
-	 * @param filename
-	 *            - the name of the file that needs to be read
+	 * @param filename - the name of the file that needs to be read
 	 */
-	public static void processLinesInFile(String filename) {
+	public static void processLinesInFile(String filename)
+	{
 
 		A1Driver myPalFinder = new A1Driver();
-		try {
+		try
+		{
 			FileReader freader = new FileReader(filename);
 			BufferedReader reader = new BufferedReader(freader);
 
-			for (String s = reader.readLine(); s != null; s = reader.readLine()) {
+			for (String s = reader.readLine(); s != null; s = reader.readLine())
+			{
 				System.out.println("The input string is: " + s);
 				String palindromes = myPalFinder.parse(s);
 				System.out.println("The Palindromes found: " + palindromes);
 			}
-		} catch (FileNotFoundException e) {
+		} catch (FileNotFoundException e)
+		{
 			System.err.println("Error: File not found. Exiting...");
 			e.printStackTrace();
 			System.exit(-1);
-		} catch (IOException e) {
+		} catch (IOException e)
+		{
 			System.err.println("Error: IO exception. Exiting...");
 			e.printStackTrace();
 			System.exit(-1);
@@ -62,60 +68,92 @@ public class A1Driver {
 	 * Parses the inputString to find all palindromes based on rules specified
 	 * in your assignment write-up.
 	 * 
-	 * @param inputString
-	 *            - the String that needs to be parsed to discover palindromes
+	 * @param inputString - The String that needs to be parsed to discover palindromes
 	 * 
 	 * @return the String object containing the Palindromes found in the
 	 *         inputString
 	 */
-	public String parse(String inputString) {
-		// modify the following code. Add/delete anything you want after this
-		// point.
-		String outputString = new String(inputString); // makes a copy of
-														// inputString.
-		// Make sure to report each palindrome ONLY ONCE
-		// TODO make sure to test with an empty line
-		System.out.println("Starting string: " + outputString);
-		outputString = filter(outputString);
-		System.out.println("After filter: " + outputString);
-		TreeSet<String> palindrome_set = findPalindromes(outputString);
-		StringBuilder sb = new StringBuilder();
-		Iterator<String> it = palindrome_set.iterator();
-		while (it.hasNext()) {
+	public String parse(String inputString)
+	{
+		String filteredString = filter(inputString);
+		TreeSet<String> palindromeSet = findPalindromes(filteredString);
+
+		// Remove middle palindromes
+		TreeSet<String> blacklist = new TreeSet<String>();
+		Iterator<String> it = palindromeSet.iterator();
+		while (it.hasNext())
+		{
 			String element = it.next();
-			System.out.println(element);
+			blacklist.addAll(findMiddlePalindromes(element));
 		}
-		return outputString;
+		palindromeSet.removeAll(blacklist);
+
+		// Reset the iterator to loop through again adding the palindromes to report
+		it = palindromeSet.iterator();
+		StringBuilder outputString = new StringBuilder();
+		String loopDelim = "";
+		while (it.hasNext())
+		{
+			String element = it.next();
+			outputString.append(loopDelim);
+			outputString.append(element);
+			loopDelim = " ";
+		}
+		return outputString.toString();
 	}
 
 	/**
 	 * Filters all punctuation and spaces out of the string, makes everything
 	 * uppercase, and returns the new string.
 	 * 
-	 * @param inputString
-	 *            - the String that needs to be filtered
+	 * @param inputString - the String that needs to be filtered
 	 * 
 	 * @return the String object that has no punctuation and is all uppercase
 	 */
-	private String filter(String inputString) {
+	private String filter(String inputString)
+	{
 		String s = inputString.replaceAll("[^a-zA-Z]", "").toUpperCase();
 		return s;
 	}
-
-	private TreeSet<String> findPalindromes(String inputString) {
-		TreeSet<String> output_set = new TreeSet<String>();
-		//for (int i = 0; i < inputString.length(); i++) {
-		//	
-		//}
-		output_set.add("Hello");
-		output_set.add("Hello");
-		output_set.add("Goodbye");
-		System.out.println(isPalindrome("Hello"));
-		System.out.println(isPalindrome("Goodbye"));
-		System.out.println(isPalindrome("HELLEH"));
-		return output_set;
-	}
 	
+	/**
+	 * Finds all of the palindromes in the input string
+	 * @param inputString
+	 * @return TreeSet<String> with all the palindromes (TreeSet is a sorted set)
+	 */
+	private TreeSet<String> findPalindromes(String inputString)
+	{
+		TreeSet<String> outputSet = new TreeSet<String>();
+		// Find all palindromes
+		for (int i = 0; i < inputString.length(); i++)
+		{
+			for (int j = i + 3; j <= inputString.length(); j++)
+			{
+				String sub = inputString.substring(i, j);
+				if (isPalindrome(sub))
+				{
+					outputSet.add(sub);
+				}
+			}
+		}
+		return outputSet;
+	}
+
+	/**
+	 * Finds all of the palindromes in the middle of another palindrome
+	 * @param inputString - palindrome to search
+	 * @return TreeSet<String> with all the middle palindromes
+	 */
+	private TreeSet<String> findMiddlePalindromes(String inputString)
+	{
+		TreeSet<String> output = new TreeSet<String>();
+		for (int i = 1; i < inputString.length() / 2; i++)
+		{
+			output.add(inputString.substring(i, inputString.length() - i));
+		}
+		return output;
+	}
+
 	/**
 	 * Determines if the given string is a palindrome. This is a case-sensitive
 	 * method.
@@ -124,12 +162,15 @@ public class A1Driver {
 	 * 
 	 * @return boolean value that tells if the String is a palindrome or note
 	 */
-	private boolean isPalindrome(String inputString) {
+	private boolean isPalindrome(String inputString)
+	{
 		String reversed = new StringBuilder(inputString).reverse().toString();
-		
-		if(reversed.equals(inputString)) {
+
+		if (reversed.equals(inputString))
+		{
 			return true;
-		} else {
+		} else
+		{
 			return false;
 		}
 	}
--- Assignment1/README.md
+++ Assignment1/README.md
@@ -1,3 +1,6 @@
 <h1>Assignment 1</h1>
 This assignment is to find palindromes hidden inside of strings.
 * Input lines are <= 60 characters
+* Ignores any characters that are not letters
+* Is not case-sensitive
+* Input file is passed on the command line
